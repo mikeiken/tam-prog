8ce8711fb353620d3d0d7d6cda7c289ee0114457
o6ez9na <o6ez9na@gmail.com>
2024-09-23 16:21:47 +0300

    create main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import AuthForm from './components/auth/auth';
+import MainPage from './components/main-page/MainPage';
+
 function App() {
   return (
     <div className="App">
-      <AuthForm/>
+      {/* <AuthForm/> */}
+      <MainPage />
     </div>
   );
 }
