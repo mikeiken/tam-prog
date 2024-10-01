6648c8c259bee2d45a040b0502dcaeec6e5f0d62
o6ez9na <kikikippplo@gmail.com>
2024-10-01 19:00:28 +0300

    test

--- frontend/src/components/add-item/AddItemButton.js
+++ frontend/src/components/add-item/AddItemButton.js
@@ -1,25 +1,13 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './add-item-button.css';
 
-export default function AddItemButton() {
-    const buttonRef = useRef(null);
-
-    const handleClick = () => {
-        // Добавляем класс вращения
-        buttonRef.current.classList.add('rotated');
-
-        // Удаляем класс после завершения анимации
-        setTimeout(() => {
-            buttonRef.current.classList.remove('rotated');
-        }, 200); // Длительность анимации
-    };
+export default function AddItemButton({ onAdd }) {
 
     return (
         <div>
             <button
-                ref={buttonRef} // Привязываем реф к кнопке
                 className='add-button'
-                onClick={handleClick}
+                onClick={onAdd}
             ></button>
         </div>
     );
--- frontend/src/components/main-page/MainPage.js
+++ frontend/src/components/main-page/MainPage.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import Header from '../header/Header'
-import Footer from '../footer/Footer'
-import Card from '../card/Card'
-import AddItemButton from '../add-item/AddItemButton'
-import './style.css'
+import React, { useState } from 'react';
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+import Card from '../card/Card';
+import AddItemButton from '../add-item/AddItemButton';
+import './style.css';
 
 export default function MainPage() {
+    const [cards, setCards] = useState([{ label: 'картошка', description: 'омерзительная тварь (нет)' }]);
+
+    const addCard = () => {
+        // Добавляем новую карточку, можно сделать динамичнее
+        setCards([...cards, { label: 'новая карточка', description: 'описание новой карточки' }]);
+    };
+
     return (
         <div>
             <Header />
@@ -14,12 +21,13 @@ export default function MainPage() {
                     наверное выбор всякой всячины
                 </div>
                 <div className='box2'>
-                    <Card label='картошка' description='омерзительная тварь (нет)' />
-                    <AddItemButton />
-
+                    {cards.map((card, index) => (
+                        <Card key={index} label={card.label + ` ${index}`} description={card.description} />
+                    ))}
+                    <AddItemButton onAdd={addCard} />
                 </div>
             </div>
             <Footer />
         </div>
-    )
+    );
 }
