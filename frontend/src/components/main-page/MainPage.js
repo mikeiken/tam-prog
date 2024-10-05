23ae31eec597bfd480110a0847f28b8b8174a3a8
o6ez9na <kikikippplo@gmail.com>
2024-10-05 22:54:00 +0300

    need fix

--- frontend/src/components/main-page/MainPage.js
+++ frontend/src/components/main-page/MainPage.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Header from '../header/Header';
 import Card from '../card/Card';
 import AddItemButton from '../add-item/AddItemButton';
+import {Routes, Route, Link} from 'react-router-dom';
+import AuthForm from '../auth/auth';
 import './style.css';
 
 export default function MainPage() {
@@ -13,19 +15,19 @@ export default function MainPage() {
     };
 
     return (
-        <div className='main-container'>
-            <Header />
-            <div className='h-container'>
-                <div className='box1'>
-                    наверное выбор всякой всячины
-                </div>
-                <div className='box2'>
-                    {cards.map((card, index) => (
-                        <Card key={index} label={card.label + ` ${index}`} description={card.description} />
-                    ))}
-                    <AddItemButton onAdd={addCard} />
-                </div>
+        <div className='h-container'>
+            <div className='box1'>
+                <Link to='/login'>Login</Link>
             </div>
+            <div className='box2'>
+                {cards.map((card, index) => (
+                    <Card key={index} label={card.label + ` ${index}`} description={card.description} />
+                ))}
+                <AddItemButton onAdd={addCard} />
+            </div>
+            <Routes>
+                <Route path='/login' element={<AuthForm />} />
+            </Routes>
         </div>
     );
 }
