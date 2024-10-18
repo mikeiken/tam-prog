60eb96105e0a69f62cf63caceaee02241e23a826
o6ez9na <kikikippplo@gmail.com>
2024-10-18 13:25:23 +0300

    fix

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import AuthForm from '../auth/auth';
 import Garden from '../main-page/Garden/Garden';
@@ -9,35 +9,34 @@ import { CSSTransition, SwitchTransition } from "react-transition-group";
 import '../main-page/style.css';
 
 export default function Header() {
-    const location = useLocation();  // Получаем текущее местоположение (маршрут)
+    const location = useLocation();
 
     const routes = [
         { path: '/', Component: Garden },
         { path: '/about', Component: About },
         { path: '/license', Component: License },
         { path: '/contractor', Component: Conractor },
+        { path: '/login', Component: AuthForm }, // Обновите маршрут для AuthForm
     ];
 
     return (
         <>
             <header className='header'>
-                <img className='logo' src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo"></img>
+                <img className='logo' src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
                 <nav className='navbar'>
                     <Link to='/'>Участки</Link>
                     <Link to='/contractor'>Подрядчики</Link>
                     <Link to='/license'>Лицензия</Link>
                     <Link to='/about'>О нас</Link>
                 </nav>
-
                 <Link to='/login'>
                     <button className='login-btn'>Войти</button>
                 </Link>
             </header>
 
-            {/* Оборачиваем в SwitchTransition для плавного переключения страниц */}
             <SwitchTransition>
                 <CSSTransition
-                    key={location.key}  // Используем уникальный ключ для каждой локации
+                    key={location.key}
                     timeout={1000}
                     classNames="main"
                     unmountOnExit
@@ -46,7 +45,6 @@ export default function Header() {
                         {routes.map(({ path, Component }) => (
                             <Route key={path} path={path} element={<Component />} />
                         ))}
-                        <Route path='/login' element={<AuthForm />} />
                     </Routes>
                 </CSSTransition>
             </SwitchTransition>
