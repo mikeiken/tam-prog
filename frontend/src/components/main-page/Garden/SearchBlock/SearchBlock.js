a4650089099a2ae0c47623ef471c705429d5ae94
o6ez9na <kikikippplo@gmail.com>
2024-10-18 14:28:58 +0300

    add search form

--- frontend/src/components/main-page/Garden/SearchBlock/SearchBlock.js
+++ frontend/src/components/main-page/Garden/SearchBlock/SearchBlock.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../garden.css';
 
 export default function SearchBlock() {
-  return (
-    <div className='box1'>
-                {/* Контент для box1 */}
-    </div>
-  )
+    return (
+        <div className='box1'>
+            <div className='search-block'>
+                <input className='search-input' placeholder='Название объекта' />
+                <img src={process.env.PUBLIC_URL + '/search.png'} alt='Search Icon' className='search-icon' />
+            </div>
+        </div>
+    );
 }
