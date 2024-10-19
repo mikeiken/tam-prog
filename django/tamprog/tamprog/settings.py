5adeb5748dc74cf5d4ad02c796a77e2230ee6f33
Nvnastya <nikolskaya_anva@edu.bstu.ru>
2024-10-20 00:28:48 +0300

    -pug+cors

--- django/tamprog/tamprog/settings.py
+++ django/tamprog/tamprog/settings.py
@@ -175,6 +175,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 30
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    #'PAGE_SIZE': 30
 }
