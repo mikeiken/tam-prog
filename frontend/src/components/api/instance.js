45c83eda2632827ce86759213f6197805c22ea52
o6ez9na <kikikippplo@gmail.com>
2024-10-19 14:35:43 +0300

    requests fix

--- frontend/src/components/api/instance.js
+++ frontend/src/components/api/instance.js
@@ -1,24 +1,22 @@
 import axios from 'axios'
 
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
+// function getCookie(name) {
+//     const value = `; ${document.cookie}`;
+//     const parts = value.split(`; ${name}=`);
+//     if (parts.length === 2) return parts.pop().split(';').shift();
+// }
 
-const xsrfToken = getCookie('XSRF-TOKEN');
+// const xsrfToken = getCookie('XSRF-TOKEN');
+// xsrfCookieName: 'XSRF-TOKEN',
+// xsrfHeaderName: 'X-XSRF-TOKEN',
 
 
 const Instance = axios.create({
     baseURL: 'http://homelab.kerasi.ru/api/v1',
     timeout: 5000,
     headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Accept': 'application/json',
-        'X-XSRF-TOKEN': xsrfToken,
+        'Content-Type': 'application/json',
     },
-    xsrfCookieName: 'XSRF-TOKEN',
-    xsrfHeaderName: 'X-XSRF-TOKEN',
 })
 
 
