c0254efc298797d95e0c5429b03a178b996aab41
Kolan4ik2003 <zarembiczkiy@mail.ru>
2024-10-19 23:18:04 +0300

    update conftest

--- django/tamprog/garden/tests/conftest.py
+++ django/tamprog/garden/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from garden.models import Agronomist, Supplier, Worker, GardenBed, Fertilizer, User, Plant, Plot, Order
+from garden.models import Agronomist, Supplier, Worker, GardenBed, Fertilizer, User, Plant, Plot, Order, AvailablePlants
 from django.utils import timezone
 
 @pytest.fixture
@@ -16,22 +16,28 @@ def worker():
 
 @pytest.fixture
 def garden_bed():
-    return GardenBed.objects.create(state="Хорошее", size=10.5)
+    return GardenBed.objects.create(state="Хорошее", size=10.5, price=10)
 
 @pytest.fixture
 def fertilizer():
-    return Fertilizer.objects.create(compound="Азот, Фосфор")
+    return Fertilizer.objects.create(compound="Азот, Фосфор",price = 10,
+        boost = 4,
+        name = "Навоз")
 
 @pytest.fixture
 def user(agronomist, worker):
     return User.objects.create(password="hashed_password", login="user123", role="admin",
-                               email="user@example.com", phone="+1234567890",
+                               email="user@mail.ru", phone="+72345678908",
                                agronomist_id=agronomist, worker_id=worker)
 
 @pytest.fixture
 def plant(garden_bed):
-    return Plant.objects.create(name="Помидор", growth_conditions="Тепло, Влажно",
-                                 nutrients="Азот, Калий", garden_id=garden_bed)
+    return Plant.objects.create(name="Помидор",
+        growth_time=24,
+        description="Великое",
+        garden_id=garden_bed,
+        price=10,
+        landing_data = "2024-09-03")
 
 @pytest.fixture
 def plot(garden_bed):
@@ -41,3 +47,14 @@ def plot(garden_bed):
 def order(garden_bed, plant, worker, fertilizer):
     return Order.objects.create(deadline="2024-10-19", garden_id=garden_bed,
                                  plant_id=plant, worker_id=worker, fertilizer_id=fertilizer)
+
+@pytest.fixture
+def available_plants(garden_bed):
+    return AvailablePlants.objects.create(
+        name="Помидор",
+        price=15.5,
+        growth_time=90,
+        description="Красный сочный овощ",
+        landing_data="2024-05-10",
+        garden_id=1
+    )
