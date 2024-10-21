2522a90827c931dadb5ab007b40382add7118896
o6ez9na <kikikippplo@gmail.com>
2024-10-21 23:25:09 +0300

    css and api fix

--- frontend/src/components/main-page/Garden/PlotInfo/PlotInfoWindow/PlotInfoWindow.js
+++ frontend/src/components/main-page/Garden/PlotInfo/PlotInfoWindow/PlotInfoWindow.js
@@ -15,9 +15,9 @@ export default function PlotInfoWindow({ item }) {
         <div className={`container-plot-wrapper ${isVisible ? 'visible' : ''}`}>
             <div className='container-plot-info'>
                 <h3>Информация об объекте:</h3>
-                <p>ID: {item.id}</p>
-                <p>Название: {item.name}</p>
-                <p>Описание: {item.description}</p>
+                <p>Название: {item.state}</p>
+                <p>Площадь: {item.size}</p>
+                <p>Цена: {item.price}</p>
             </div>
 
             <div className='container-plot-products'>
