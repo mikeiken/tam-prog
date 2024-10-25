c4bbd737988a0b5b07e746325842514acecf9b83
Nvnastya <nikolskaya_anva@edu.bstu.ru>
2024-10-25 23:32:13 +0300

    start

--- django/tamprog/garden/views.py
+++ django/tamprog/garden/views.py
@@ -24,65 +24,30 @@ class AgronomistViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = Agronomist.objects.all()
     serializer_class = AgronomistSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
 
 
 class SupplierViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
 
 
 class WorkerViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = Worker.objects.all()
     serializer_class = WorkerSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
 
 
 class GardenBedViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = GardenBed.objects.all()
     serializer_class = GardenBedSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
 
 
 class FertilizerViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = Fertilizer.objects.all()
     serializer_class = FertilizerSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
     
     @action(detail=False, methods=['get'], url_path='search')
     def search(self, request):
@@ -111,26 +76,12 @@ class UserViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
 
 
 class PlantViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
     
     @action(detail=False, methods=['get'], url_path='search')
     def search(self, request):
@@ -159,38 +110,15 @@ class PlotViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = Plot.objects.all()
     serializer_class = PlotSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
-
 
 class OrderViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
 
 class AvailablePlantsViewSet(CORSMixin, viewsets.ModelViewSet):
     queryset = AvailablePlants.objects.all()
     serializer_class = AvailablePlantsSerializer
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response()
 
     @action(detail=False, methods=['get'], url_path='search')
     def search(self, request):
