a346754c5f7f47cdab6fbd698fea8f733ca61cc3
o6ez9na <kikikippplo@gmail.com>
2024-11-05 15:00:41 +0300

    navigate fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,23 @@ import MainPage from './components/main-page/MainPage';
 import AuthForm from './components/auth/auth';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/header/Header';
+import NotFound from './components/not-found/NotFound';
+import Garden from './components/main-page/Garden/Garden';
 
 function App() {
-  const location = useLocation(); // Получаем текущий путь
+  const location = useLocation();
+
+  // Header отображается только на конкретных страницах
+  const showHeader = !["/", "/login"].includes(location.pathname);
 
   return (
     <div className="App">
-      {location.pathname !== "/" && <Header />}
 
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<AuthForm />} />
+        <Route path="/navigate/*" element={<Header />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -5,16 +5,17 @@ import About from '../main-page/About/about';
 import License from '../main-page/License/License';
 import Contractor from '../main-page/Contractor/Contractor';
 import { CSSTransition, SwitchTransition } from "react-transition-group";
+import NotFound from '../not-found/NotFound';
 import '../main-page/style.css';
 
 export default function Header() {
     const location = useLocation();
 
     const routes = [
-        { path: '/garden', Component: Garden },
-        { path: '/about', Component: About },
-        { path: '/license', Component: License },
-        { path: '/contractor', Component: Contractor },
+        { path: 'garden', Component: Garden },
+        { path: 'about', Component: About },
+        { path: 'license', Component: License },
+        { path: 'contractor', Component: Contractor },
     ];
 
     const isActivePath = (path) => location.pathname === path;
@@ -28,25 +29,25 @@ export default function Header() {
                 <img className='logo' src={process.env.PUBLIC_URL + '/logo.png'} alt="Logo" />
                 <nav className='navbar'>
                     <Link
-                        to='/garden'
+                        to='garden'
                         onClick={e => isActivePath('/garden') && e.preventDefault()}
                     >
                         Участки
                     </Link>
                     <Link
-                        to='/contractor'
+                        to='contractor'
                         onClick={e => isActivePath('/contractor') && e.preventDefault()}
                     >
                         Подрядчики
                     </Link>
                     <Link
-                        to='/license'
+                        to='license'
                         onClick={e => isActivePath('/license') && e.preventDefault()}
                     >
                         Ваши участки
                     </Link>
                     <Link
-                        to='/about'
+                        to='about'
                         onClick={e => isActivePath('/about') && e.preventDefault()}
                     >
                         О нас
