b11fd4fcc6f44149df6c9de9e5d2562c49f17559
kseen <korenevdn@gmail.com>
2024-11-07 20:36:44 +0300

    Remove obsolete cleanup scripts and update Docker Compose configuration for PostgreSQL service

--- cleanup.ps1
+++
@@ -1,6 +0,0 @@
-# delete build folders
-Write-Host "Deleting nginx build-container folder..."
-Remove-Item -Path .\nginx\build-container -Recurse -Force
-
-Write-Host "Deleting psql build-container folder..."
-Remove-Item -Path .\psql\build-container -Recurse -Force
