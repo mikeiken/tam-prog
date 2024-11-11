2a32f19d7dab451965c8b8f95f93118d357c3c76
kseen <korenevdn@gmail.com>
2024-11-11 22:28:07 +0300

    Enhance BedPlantFertilizerViewSet with detailed OpenAPI schema for list, create, partial update, destroy, retrieve, and update actions

--- django/tamprog/fertilizer/views.py
+++ django/tamprog/fertilizer/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Fertilizer, BedPlantFertilizer
@@ -27,6 +27,123 @@ class BedPlantFertilizerViewSet(viewsets.ModelViewSet):
     serializer_class = BedPlantFertilizerSerializer
     permission_classes = [BedPlantF]
 
+    @extend_schema(
+        summary='Get fertilizers applied to a plant',
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='Successfull response',
+                response=BedPlantFertilizerSerializer(many=True)
+            )
+        },
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Apply fertilizer to a plant',
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='Successfull response',
+            )
+        },
+        parameters=[
+            OpenApiParameter(
+                name='bed_plant',
+                description='Bed plant ID',
+                type=int,
+                required=True,
+            ),
+            OpenApiParameter(
+                name='fertilizer',
+                description='Fertilizer ID',
+                type=int,
+                required=True,
+            ),
+        ],
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Update fertilizer applied to a plant',
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='Successfull response',
+            )
+        },
+        parameters=[
+            OpenApiParameter(
+                name='bed_plant',
+                description='Bed plant ID',
+                type=int,
+            ),
+            OpenApiParameter(
+                name='fertilizer',
+                description='Fertilizer ID',
+                type=int,
+            ),
+        ],
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Delete fertilizer applied to a plant',
+        responses={
+            status.HTTP_204_NO_CONTENT: OpenApiResponse(
+                description='Successfull response',
+            )
+        },
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+    
+    @extend_schema(
+        summary='Get fertilizer applied to a plant',
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='Successfull response',
+                response=BedPlantFertilizerSerializer
+            )
+        },
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    @extend_schema(
+        summary='Update fertilizer applied to a plant',
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='Successfull response',
+            )
+        },
+        parameters=[
+            OpenApiParameter(
+                name='bed_plant',
+                description='Bed plant ID',
+                type=int,
+                required=True,
+            ),
+            OpenApiParameter(
+                name='fertilizer',
+                description='Fertilizer ID',
+                type=int,
+                required=True,
+            ),
+        ],
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        summary='Get fertilizers applied to a plant',
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                description='Successfull response',
+                response=BedPlantFertilizerSerializer(many=True)
+            )
+        },
+    )
     @action(detail=True, methods=['get'])
     def fertilizers(self, request, pk=None):
         bed_plant = self.get_object()
