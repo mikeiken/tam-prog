cf23ebecfa8695671f15741fe2c4c511cc3543ab
kseen <korenevdn@gmail.com>
2024-11-18 01:56:55 +0300

    Refactor environment injection scripts to improve command handling and enhance .env file loading

--- docker-compose-build-up.ps1
+++ docker-compose-build-up.ps1
@@ -1,4 +1,3 @@
 # $command = $args -join " "
 ./env-inject.ps1
 docker-compose --env-file ./.env -f docker-compose-build.yml up -d $args
-# ./env-inject.ps1 docker-compose -f docker-compose-build.yml up -d
--- docker-compose-up.ps1
+++ docker-compose-up.ps1
@@ -1,3 +1,2 @@
 ./env-inject.ps1
 docker-compose --env-file ./.env up -d $args
-# ./env-inject.ps1 docker-compose up -d
