b0d88adf78635dd87aa0a256b92b44fa10a44b7a
Kolan4ik2003 <zarembiczkiy@mail.ru>
2024-11-22 01:48:43 +0300

    update tests

--- django/tamprog/user/tests.py
+++ django/tamprog/user/tests.py
@@ -8,101 +8,75 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @pytest.mark.django_db
-def test_register_user(api_client):
+def test_register_user(api_client, register_data):
     """Тест успешной регистрации нового пользователя."""
     url = '/api/v1/register/'
-    data = {
-        'username': 'newuser',
-        'phone_number': '+1234567890',
-        'full_name': 'New User',
-        'password': 'newpassword',
-        'wallet_balance': 100.00
-    }
-    response = api_client.post(url, data, format='json')
+    response = api_client.post(url, register_data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert User.objects.filter(username='newuser').exists()
+    assert User.objects.filter(username=register_data['username']).exists()
+
 
 @pytest.mark.django_db
-def test_register_user_existing_username(api_client, user):
+def test_register_user_existing_username(api_client, register_data, user):
     """Тест на регистрацию пользователя с уже существующим именем."""
+    register_data['username'] = user.username
     url = '/api/v1/register/'
-    data = {
-        'username': user.username,
-        'phone_number': '+1234567890',
-        'full_name': 'New User',
-        'password': 'newpassword',
-        'wallet_balance': 50.00
-    }
-    response = api_client.post(url, data, format='json')
+    response = api_client.post(url, register_data, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert "username" in response.data
+
 
 @pytest.mark.django_db
 def test_register_user_missing_fields(api_client):
     """Тест на регистрацию без обязательных полей."""
     url = '/api/v1/register/'
-    data = {
-        'username': 'incompleteuser'
-        # Пропущены обязательные поля
-    }
-    response = api_client.post(url, data, format='json')
+    response = api_client.post(url, {}, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert "username" in response.data
     assert "phone_number" in response.data
     assert "password" in response.data
 
+
 @pytest.mark.django_db
-def test_register_user_invalid_phone_number(api_client):
+def test_register_user_invalid_phone_number(api_client, register_data):
     """Тест на регистрацию с неверным форматом номера телефона."""
+    register_data['phone_number'] = 'invalid_phone'
     url = '/api/v1/register/'
-    data = {
-        'username': 'user_invalid_phone',
-        'phone_number': 'invalid_phone',
-        'full_name': 'New User',
-        'password': 'newpassword',
-        'wallet_balance': 100.00
-    }
-    response = api_client.post(url, data, format='json')
+    response = api_client.post(url, register_data, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert "phone_number" in response.data
+
 
 @pytest.mark.django_db
 def test_login_user(api_client, user):
     """Тест успешного входа с корректными данными."""
-    user.set_password('testpassword')
-    user.save()
     url = '/api/v1/login/'
-    data = {
-        'username': user.username,
-        'password': 'testpassword'
-    }
+    data = {'username': user.username, 'password': 'testpassword'}
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_200_OK
     assert 'access' in response.data
     assert 'refresh' in response.data
     assert 'wallet_balance' in response.data
 
+
 @pytest.mark.django_db
 def test_login_user_invalid_credentials(api_client):
     """Тест на вход с неверными данными."""
     url = '/api/v1/login/'
-    data = {
-        'username': 'nonexistentuser',
-        'password': 'wrongpassword'
-    }
+    data = {'username': 'nonexistentuser', 'password': 'wrongpassword'}
     response = api_client.post(url, data, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert "detail" in response.data
     assert response.data["detail"] == "Invalid credentials"
 
+
 @pytest.mark.django_db
 def test_login_user_missing_fields(api_client):
     """Тест на вход с отсутствующими обязательными полями."""
     url = '/api/v1/login/'
-    data = {
-        'username': 'testuser'
-    }
-    response = api_client.post(url, data, format='json')
+    response = api_client.post(url, {}, format='json')
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert "username" in response.data
     assert "password" in response.data
 
 @pytest.mark.django_db
