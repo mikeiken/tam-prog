ae99172e8139a668bab3f82679dd97b847e8ec8f
o6ez9na <kikikippplo@gmail.com>
2024-11-24 16:49:18 +0300

    add new form registration

--- frontend/src/components/auth/register/RegisterForm.js
+++ frontend/src/components/auth/register/RegisterForm.js
@@ -52,108 +52,106 @@ export default function RegisterForm() {
 
   return (
     <div className="intro">
-      <div className="video">
-        <img
-          src={process.env.PUBLIC_URL + "/tenor.gif"}
-          alt="Background GIF"
-          className="background-video"
-        />
-      </div>
+      <div className="auth-page-wrapper">
+        <div className="waves"></div>
+        <div className="waves"></div>
+        <div className="waves"></div>
 
-      <div className="main-wrapper">
-        <div className="wrapper">
-          {showAlert && (
-            <Alert text={errorMessage} className={showAlert ? "" : "hide"} />
-          )}
+        <div className="main-wrapper">
+          <div className="wrapper">
+            {showAlert && (
+              <Alert text={errorMessage} className={showAlert ? "" : "hide"} />
+            )}
 
-          <form onSubmit={handleSubmit}>
-            <h1>Register</h1>
-            <div className="input-box">
-              <input
-                className=""
-                type="text"
-                required
-                onChange={handleChange(setUsername)}
-              />
-              <label>Enter your name</label>
-              <img
-                className="user-icon-auth"
-                src={process.env.PUBLIC_URL + "/user.png"}
-                alt="text"
-              ></img>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <h1>Register</h1>
+              <div className="input-box">
+                <input
+                  className=""
+                  type="text"
+                  required
+                  onChange={handleChange(setUsername)}
+                />
+                <label>Enter your name</label>
+                <img
+                  className="user-icon-auth"
+                  src={process.env.PUBLIC_URL + "/user.png"}
+                  alt="text"
+                ></img>
+              </div>
 
-            <div className="input-box">
-              <input
-                className=""
-                type="text"
-                required
-                onChange={handleChange(setLogin)}
-              />
-              <label>Enter your login</label>
-              <img
-                className="user-icon-auth"
-                src={process.env.PUBLIC_URL + "/user.png"}
-                alt="text"
-              ></img>
-            </div>
+              <div className="input-box">
+                <input
+                  className=""
+                  type="text"
+                  required
+                  onChange={handleChange(setLogin)}
+                />
+                <label>Enter your login</label>
+                <img
+                  className="user-icon-auth"
+                  src={process.env.PUBLIC_URL + "/user.png"}
+                  alt="text"
+                ></img>
+              </div>
 
-            <div className="input-box">
-              <input
-                className=""
-                type="phone"
-                required
-                onChange={handleChange(setPhone)}
-              />
-              <label>Enter your phone</label>
-              <img
-                className="user-icon-auth"
-                src={process.env.PUBLIC_URL + "/phone.png"}
-                alt="text"
-              ></img>
-            </div>
+              <div className="input-box">
+                <input
+                  className=""
+                  type="phone"
+                  required
+                  onChange={handleChange(setPhone)}
+                />
+                <label>Enter your phone</label>
+                <img
+                  className="user-icon-auth"
+                  src={process.env.PUBLIC_URL + "/phone.png"}
+                  alt="text"
+                ></img>
+              </div>
 
-            <div className="input-box">
-              <input
-                className=""
-                type="password"
-                required
-                onChange={handleChange(setPasswordFirst)}
-              />
-              <label>Enter your password</label>
-              <img
-                className="user-icon-auth"
-                src={process.env.PUBLIC_URL + "/key-chain.png"}
-                alt="text"
-              ></img>
-            </div>
+              <div className="input-box">
+                <input
+                  className=""
+                  type="password"
+                  required
+                  onChange={handleChange(setPasswordFirst)}
+                />
+                <label>Enter your password</label>
+                <img
+                  className="user-icon-auth"
+                  src={process.env.PUBLIC_URL + "/key-chain.png"}
+                  alt="text"
+                ></img>
+              </div>
 
-            <div className="input-box">
-              <input
-                className=""
-                type="password"
-                required
-                onChange={handleChange(setPasswordSecond)}
-              />
-              <label>Repeat your password</label>
-              <img
-                className="user-icon-auth"
-                src={process.env.PUBLIC_URL + "/key-chain.png"}
-                alt="text"
-              ></img>
-            </div>
+              <div className="input-box">
+                <input
+                  className=""
+                  type="password"
+                  required
+                  onChange={handleChange(setPasswordSecond)}
+                />
+                <label>Repeat your password</label>
+                <img
+                  className="user-icon-auth"
+                  src={process.env.PUBLIC_URL + "/key-chain.png"}
+                  alt="text"
+                ></img>
+              </div>
 
-            <button type="submit" className="btn">
-              Register
-            </button>
-            <div className="register-link">
-              <Link to="/">← Go back</Link>
-            </div>
-          </form>
+              <button type="submit" className="btn">
+                Register
+              </button>
+              <div className="register-link">
+                <Link to="/">← Go back</Link>
+              </div>
+            </form>
 
-          <Routes>
-            <Route path="/login" element={<AuthForm />} />
-          </Routes>
+            <Routes>
+              <Route path="/login" element={<AuthForm />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </div>
