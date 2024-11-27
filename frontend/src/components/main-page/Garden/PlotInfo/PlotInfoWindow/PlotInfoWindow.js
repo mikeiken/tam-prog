d09fde8d6c11c3653770c85ce667e38e6a8c2c0a
o6ez9na <kikikippplo@gmail.com>
2024-11-28 00:42:33 +0300

    add new styles

--- frontend/src/components/main-page/Garden/PlotInfo/PlotInfoWindow/PlotInfoWindow.js
+++ frontend/src/components/main-page/Garden/PlotInfo/PlotInfoWindow/PlotInfoWindow.js
@@ -1,29 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
-export default function PlotInfoWindow({ item }) {
-  const [isVisible, setIsVisible] = useState(false);
+export default function PlotInfoWindow({item}) {
+    const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    if (item) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  }, [item]);
+    useEffect(() => {
+        if (item) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    }, [item]);
 
-  return (
-    <div className={`container-plot-wrapper ${isVisible ? "visible" : ""}`}>
-      <div className="container-plot-info">
-        <h3>Информация об объекте:</h3>
-        <p>Название: {item.name}</p>
-        <p>Площадь: {item.all_beds} грядок</p>
-        <p>Свободных грядок: {item.count_free_beds} грядок</p>
-        <p>Цена: {item.price}</p>
-      </div>
+    return (
+        <div className={`container-plot-wrapper ${isVisible ? "visible" : ""}`}>
+            <div className="container-plot-info">
+                <div className={"container-wrapper-info"}>
+                    <div className={"container-wrapper-img"}>
+                        <img
+                            className="plot-card-img"
+                            src="https://avatars.mds.yandex.net/i?id=b4560f26cb2f9fab2f7395b28c1fed50_l-5236855-images-thumbs&n=13"
+                            alt="img"
+                        ></img>
+                    </div>
+                    <div className={"container-for-text"}>
+                        <h3>Лучшее решение для начинающего предпринимателя!</h3>
+                        <ul>
+                            <li>
+                                Минимальные обязательства
+                            </li>
+                            <li>
+                                Простое обслуживание
+                            </li>
+                            <li>
+                                Идеально для сезонного использования
+                            </li>
+                            <li>
+                                Цена: {item.price}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-      <div className="container-plot-products">
-        <h1>ID: {item.id}</h1>
-      </div>
-    </div>
-  );
+            <div className="container-plot-products">
+                <h1>ID: {item.id}</h1>
+            </div>
+        </div>
+    );
 }
--- frontend/src/components/main-page/Garden/SearchCard/SearchCard.js
+++ frontend/src/components/main-page/Garden/SearchCard/SearchCard.js
@@ -1,30 +1,33 @@
 import React from "react";
-import { useBasket } from "../../../basket-context/BasketContext";
-export default function SearchCard({ item, onClick }) {
-  const { addToBasket } = useBasket();
-  return (
-    <div className="search-card-wrapper" onClick={onClick}>
-      <div className="search-card-wrapper-v2">
-        <img
-          className="search-card-img"
-          src="https://avatars.mds.yandex.net/i?id=b4560f26cb2f9fab2f7395b28c1fed50_l-5236855-images-thumbs&n=13"
-          alt="img"
-        ></img>
-        <div>
-          <p className="text-into-search-card">
-            {item.name}, {item.id}
-            <br /> Cвободно {item.count_free_beds} грядок
-            <br /> Цена: {item.price} Рублей
-          </p>
+import {useBasket} from "../../../basket-context/BasketContext";
+
+export default function SearchCard({item, onClick}) {
+    const {addToBasket} = useBasket();
+    return (
+        <div className="search-card-wrapper" onClick={onClick}>
+            <div className="search-card-wrapper-v2">
+                <img
+                    className="search-card-img"
+                    src="https://avatars.mds.yandex.net/i?id=b4560f26cb2f9fab2f7395b28c1fed50_l-5236855-images-thumbs&n=13"
+                    alt="img"
+                ></img>
+                <div>
+                    <h3 className={"header-into-card"}>{item.name}, {item.id}</h3>
+
+                    <div>
+                            Cвободно {item.count_free_beds} грядок
+                            <br/>
+                            Цена: {item.price} Рублей / грядка
+                    </div>
+                </div>
+            </div>
+            <div className="add-to-basket" onClick={() => addToBasket(item)}>
+                <img
+                    className="add-to-basket-img"
+                    src={process.env.PUBLIC_URL + "/plus.svg"}
+                    alt="img"
+                ></img>
+            </div>
         </div>
-      </div>
-      <div className="add-to-basket" onClick={() => addToBasket(item)}>
-        <img
-          className="add-to-basket-img"
-          src={process.env.PUBLIC_URL + "/plus.svg"}
-          alt="img"
-        ></img>
-      </div>
-    </div>
-  );
+    );
 }
