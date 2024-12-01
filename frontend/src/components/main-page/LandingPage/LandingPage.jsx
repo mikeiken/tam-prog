a280b4aa211703be17fa10341b0369186bb1ded9
o6ez9na <kikikippplo@gmail.com>
2024-12-01 23:20:28 +0300

    new landing page init

---
+++ frontend/src/components/main-page/LandingPage/LandingPage.jsx
@@ -0,0 +1,26 @@
+// import { useState } from "react";
+import "./main.css";
+import Garden from "../Garden/Garden";
+// import LoginBtn from "../ui/login-btn/LoginBtn";
+// import RegisterBtn from "../ui/register-btn/RegisterBtn";
+
+export default function LandingPage() {
+
+    return (
+        <>
+            <div className={"landing-main-wrapper"}>
+                <div className={"section1"}>
+                    <div className={"parallax-text"}>TAMPROG</div>
+                </div>
+                <div className={"section2"}>
+                    <div className={"parallax-text"}>NORMAL</div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+{/*<LoginBtn/>*/
+}
+{/*<RegisterBtn/>*/
+}
