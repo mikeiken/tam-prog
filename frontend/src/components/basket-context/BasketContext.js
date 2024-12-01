466cfcd4f55aabf624b293319030b1c8165b26ca
o6ez9na <kikikippplo@gmail.com>
2024-12-02 00:04:48 +0300

    basket fix

--- frontend/src/components/basket-context/BasketContext.js
+++ frontend/src/components/basket-context/BasketContext.js
@@ -1,6 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
 
-// Создаем контекст
 const BasketContext = createContext();
 
 export const BasketProvider = ({ children }) => {
@@ -11,32 +10,27 @@ export const BasketProvider = ({ children }) => {
 
   const [basketItems, setBasketItems] = useState(loadBasketItems);
 
-  // Сохраняем корзину в localStorage при изменении состояния
+  // Сохраняем корзину в localStorage при любом изменении состояния
   useEffect(() => {
-    if (basketItems.length > 0) {
-      localStorage.setItem("basketItems", JSON.stringify(basketItems));
-    }
+    localStorage.setItem("basketItems", JSON.stringify(basketItems));
   }, [basketItems]);
 
   const addToBasket = (item) => {
-    const currentDate = new Date().toLocaleDateString("ru-RU"); // Получаем дату в формате "день.месяц.год"
+    const currentDate = new Date().toLocaleDateString("ru-RU");
 
     setBasketItems((prevItems) => {
-      // Проверяем, есть ли уже такой элемент в корзине
       const exists = prevItems.some((i) => i.id === item.id);
       if (exists) {
-        // Если элемент уже есть, просто возвращаем текущий список
         return prevItems.map((i) =>
             i.id === item.id ? { ...i, dateAdded: currentDate } : i
         );
       }
-      // Если элемента нет, добавляем его
       return [...prevItems, { ...item, dateAdded: currentDate }];
     });
   };
 
   const removeFromBasket = (id) => {
-    setBasketItems((prevItems) => prevItems.filter((item) => item.id !== id)); // Удаляем элемент по id
+    setBasketItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
   const totalItems = basketItems.length;
@@ -46,19 +40,18 @@ export const BasketProvider = ({ children }) => {
   };
 
   return (
-    <BasketContext.Provider
-      value={{
-        totalItems,
-        basketItems,
-        addToBasket,
-        removeFromBasket,
-        clearBasket
-      }}
-    >
-      {children}
-    </BasketContext.Provider>
+      <BasketContext.Provider
+          value={{
+            totalItems,
+            basketItems,
+            addToBasket,
+            removeFromBasket,
+            clearBasket,
+          }}
+      >
+        {children}
+      </BasketContext.Provider>
   );
 };
 
-// Хук для использования контекста
 export const useBasket = () => useContext(BasketContext);
